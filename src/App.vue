48037c987349fb76d7de0e9dde79d77f0003841c
darkmasta <alperen_bagci@hotmail.com>
2021-06-02 04:22:50 +0300

    Fix the links

--- src/App.vue
+++ src/App.vue
@@ -141,13 +141,13 @@
               </button>
             </li>
             <li>
-              <a rel="noopener noreferrer" target="_blank" href="https://dribbble.com">
+              <a rel="noopener noreferrer" target="_blank" href="http://mobapassion.herokuapp.com/#/">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z"/></svg>
               </a>
             </li>
           </ul>
         </div>
-        <h5><a rel="noopener noreferrer" target="_blank" href="https://dribbble.com">Moba Passion</a></h5>
+        <h5><a rel="noopener noreferrer" target="_blank" href="http://mobapassion.herokuapp.com/#/">Moba Passion</a></h5>
         <h6>This project was my first entrepreneurship venture. Fully functional betting website with crypto deposits.</h6>
         <h6 class="mt-2">Uses ForgingBlock API for crypto deposits and withdrawls.</h6>
         <h6 class="mt-2">Frontend architecture created with AngularJS.</h6>
@@ -165,13 +165,13 @@
               </button>
             </li>
             <li>
-              <a rel="noopener noreferrer" target="_blank" href="https://dribbble.com">
+              <a rel="noopener noreferrer" target="_blank" href="https://klinikadent.herokuapp.com/">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z"/></svg>
               </a>
             </li>
           </ul>
         </div>
-        <h5><a rel="noopener noreferrer" target="_blank" href="https://dribbble.com">KlinikaDent Website</a></h5>
+        <h5><a rel="noopener noreferrer" target="_blank" href="https://klinikadent.herokuapp.com/">KlinikaDent Website</a></h5>
         <h6>An example of my freelance work.</h6>
         <h6 class="mt-2">Page is fully responsive. No CSS framework is used.</h6>
       </div>
@@ -187,13 +187,13 @@
               </button>
             </li>
             <li>
-              <a rel="noopener noreferrer" target="_blank" href="https://dribbble.com">
+              <a rel="noopener noreferrer" target="_blank" href="https://defensehere.herokuapp.com">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z"/></svg>
               </a>
             </li>
           </ul>
         </div>
-        <h5><a rel="noopener noreferrer" target="_blank" href="https://dribbble.com">Defensehere Admin Pages</a></h5>
+        <h5><a rel="noopener noreferrer" target="_blank" href="https://defensehere.herokuapp.com">Defensehere Admin Pages</a></h5>
         <h6>Defensehere is a military news website. Their entire news management system rebuilt by me.</h6>
         <h6 class="mt-2">VueJS used on frontend. NodeJS at the backend. Uses Mongo as database.</h6>
         <h6 class="mt-2"></h6>
@@ -210,13 +210,13 @@
               </button>
             </li>
             <li>
-              <a rel="noopener noreferrer" target="_blank" href="https://dribbble.com">
+              <a rel="noopener noreferrer" target="_blank" href="https://vape-cookbook.herokuapp.com">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z"/></svg>
               </a>
             </li>
           </ul>
         </div>
-        <h5><a rel="noopener noreferrer" target="_blank" href="https://dribbble.com">Vape CookBook</a></h5>
+        <h5><a rel="noopener noreferrer" target="_blank" href="https://vape-cookbook.herokuapp.com">Vape CookBook</a></h5>
         <h6>A website for DIY e-cig liquid recipes. Users can like, comment, publish recipes.</h6>
         <h6 class="mt-2">Made with Ruby on Rails. Uses PostgreSql as DB.</h6>
       </div>
@@ -232,13 +232,13 @@
               </button>
             </li>
             <li>
-              <a rel="noopener noreferrer" target="_blank" href="https://dribbble.com">
+              <a rel="noopener noreferrer" target="_blank" href="http://herbalife.herokuapp.com">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.188 8.719c.439-.439.926-.801 1.444-1.087 2.887-1.591 6.589-.745 8.445 2.069l-2.246 2.245c-.644-1.469-2.243-2.305-3.834-1.949-.599.134-1.168.433-1.633.898l-4.304 4.306c-1.307 1.307-1.307 3.433 0 4.74 1.307 1.307 3.433 1.307 4.74 0l1.327-1.327c1.207.479 2.501.67 3.779.575l-2.929 2.929c-2.511 2.511-6.582 2.511-9.093 0s-2.511-6.582 0-9.093l4.304-4.306zm6.836-6.836l-2.929 2.929c1.277-.096 2.572.096 3.779.574l1.326-1.326c1.307-1.307 3.433-1.307 4.74 0 1.307 1.307 1.307 3.433 0 4.74l-4.305 4.305c-1.311 1.311-3.44 1.3-4.74 0-.303-.303-.564-.68-.727-1.051l-2.246 2.245c.236.358.481.667.796.982.812.812 1.846 1.417 3.036 1.704 1.542.371 3.194.166 4.613-.617.518-.286 1.005-.648 1.444-1.087l4.304-4.305c2.512-2.511 2.512-6.582.001-9.093-2.511-2.51-6.581-2.51-9.092 0z"/></svg>
               </a>
             </li>
           </ul>
         </div>
-        <h5><a rel="noopener noreferrer" target="_blank" href="https://dribbble.com">ECommerce Website with Spree</a></h5>
+        <h5><a rel="noopener noreferrer" target="_blank" href="http://herbalife.herokuapp.com">ECommerce Website with Spree</a></h5>
         <h6>Fully functional Ecommerce website built with Ruby on Rails Spree gem.</h6>
         <h6 class="mt-2"></h6>
       </div>
